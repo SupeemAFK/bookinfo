1875b59c1bfaddd215beabd3742cf5f478c4ad24
SupeemAFK <Peemlomsak@gmail.com>
2025-12-15 17:51:29 +0700

    fix: compose

--- src/ratings/databases/script.sh
+++ src/ratings/databases/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -e
-mongoimport --host localhost --db ratings \
+mongoimport --host localhost --username $MONGODB_USERNAME --pasword $MONGODB_PASSWORD --db $MONGODB_DATABASE \
   --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
